741aa0dd193628d08305e47855340831b2171ef5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-29 20:31:06 -0500

    feat: Add utility functions for common actions
    These utility functions list the activitiesin the log file, and show an
    interactive activity picker to add to the log file.
    
    I have also added a case statement to handle user commands.

--- timesheet.sh
+++ timesheet.sh
@@ -42,3 +42,47 @@ process_file() {
       }
     }' "$filename"
 }
+
+list() {
+   awk -F'\t' '
+    {
+      activity = $1
+      duration = $2
+
+      activities[activity] += duration
+    }
+    END {
+      printf "Time     Activity\n"
+      for (activity in activities) {
+        duration = activities[activity]
+        hours = duration / 3600
+        minutes = (duration % 3600) / 60
+        seconds = duration % 60
+        printf "%02d:%02d:%02d %s\n", hours, minutes, seconds, activity
+      }
+    }'
+}
+
+interactive() {
+  activity=$({
+    tail -n 1 "$filename" | awk -F'\t' '{print $3}'
+    list_activities
+  } | rofi -dmenu -p "Select Activity")
+  if [ -n "$activity" ]; then
+    add_activity "$activity"
+  fi
+}
+
+case "$cmd" in
+  "add")
+    activity="$2"
+    add_activity "$activity"
+    ;;
+  *)
+    echo "Usage: $0 <command>"
+    echo
+    echo "Commands:"
+    echo "  add <activity>    Add a new activity"
+    exit 1
+    ;;
+esac
